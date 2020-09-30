2d9deaa8ffe61c056ebae2dbee15e1858ecea616
korbutds <sys@hohou.ru>
2020-09-30 14:53:26 +0300

    Реализовал функционал задачи

--- js/main.js
+++ js/main.js
@@ -61,7 +61,7 @@ const createAdsArray = (count) => {
         guests: getRandomIntInclusive(1, 10),
         checkin: getRandomArrayElement(checkTimes),
         checkout: getRandomArrayElement(checkTimes),
-        features: features.slice(getRandomIntInclusive(0, features.length - 1)),
+        features: features.slice(0, getRandomIntInclusive(1, features.length - 1)),
         description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
         photos: photos.slice(getRandomIntInclusive(0, photos.length - 1))
       },
@@ -97,3 +97,51 @@ const mapSection = document.querySelector(`.map__pins`);
 orderMap.classList.remove(`map--faded`);
 
 mapSection.appendChild(createPinFragment(createAdsArray(PIN_NUMBER)));
+
+const cardPopupTemplate = document.querySelector(`#card`).content.querySelector(`.popup`);
+
+const createAdCard = (ad) => {
+  const cardFragment = document.createDocumentFragment();
+  const newCard = cardPopupTemplate.cloneNode(true);
+  const popupTitle = newCard.querySelector(`.popup__title`);
+  const popupAddress = newCard.querySelector(`.popup__text--address`);
+  const popupPrice = newCard.querySelector(`.popup__text--price`);
+  const popupType = newCard.querySelector(`.popup__type`);
+  const popupCapacity = newCard.querySelector(`.popup__text--capacity`);
+  const popupCheck = newCard.querySelector(`.popup__text--time`);
+  const popupFeature = newCard.querySelector(`.popup__features`);
+  const popupFeatures = popupFeature.querySelectorAll(`.popup__feature`);
+  const popupPhotos = newCard.querySelector(`.popup__photos`);
+  const popupPhoto = popupPhotos.querySelectorAll(`.popup__photo`);
+  const popupAvator = newCard.querySelector(`.popup__avatar`);
+  popupTitle.textContent = ad.offer.title;
+  popupAddress.textContent = ad.offer.address;
+  popupPrice.textContent = `${ad.offer.price}₽/ночь`;
+  popupType.textContent = ad.offer.type;
+  popupCapacity.textContent = `${ad.offer.rooms} комнат для ${ad.offer.guests} гостей`;
+  popupCheck.textContent = `Заезд после ${ad.offer.checkin} выезд до ${ad.offer.checkout}`;
+
+  for (let i = 0; i < ad.offer.features.length; i++) {
+    popupFeature.children[i].textContent = ad.offer.features[i];
+  }
+
+  for (let j = 0; j < ad.offer.photos.length; j++) {
+    const newPhoto = popupPhoto[0].cloneNode(true);
+    newPhoto.src = ad.offer.photos[j];
+    popupPhotos.appendChild(newPhoto);
+  }
+  popupPhotos.removeChild(popupPhoto[0]);
+
+  for (let k = 0; k < popupFeatures.length; k++) {
+    if (!popupFeatures[k].textContent) {
+      popupFeature.removeChild(popupFeatures[k]);
+    }
+  }
+
+  popupAvator.src = ad.author.avatar;
+  cardFragment.appendChild(newCard);
+
+  return cardFragment;
+};
+
+orderMap.insertBefore(createAdCard(createAdsArray(1)[0]), orderMap.querySelector(`.map__filters-container`));
