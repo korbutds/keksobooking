a79adaf85cfee8f3c802d5f6c0a7bafc9318e3e0
korbutds <sys@hohou.ru>
2020-10-22 18:45:11 +0300

    Реализация логики сортировки

--- js/filter.js
+++ js/filter.js
@@ -18,39 +18,79 @@
   // ];
 
   const mapFilters = document.querySelector(`.map__filters`);
-
-  mapFilters.addEventListener(`change`, (evt) => {
-    let pins = window.data.serverData.slice();
-    window.pin.getRemovePopup();
-    if (evt.target.id === `housing-type`) {
-      if (evt.target.value !== `any`) {
-        window.pin.getRemovePins();
-
-        const sortedPins = pins.filter((pin) => {
-          return pin.offer.type === evt.target.value;
-        });
-
-        window.map.getPinMap(sortedPins);
-      } else {
-        window.pin.getRemovePins();
-        window.map.getPinMap(pins);
+  const f = () => {
+    console.log(`Средний ценовой`);
+  };
+  const selectObject = {
+    'housing-type': {
+      palace: `palace obj`,
+      flat: `flat obj`,
+      house: `house obj`,
+      bungalow: `bungalow obj`
+    },
+    'housing-price': {
+      middle: () => {
+        console.log(`Средний ценовой`);
+      },
+      low: () => {
+        console.log(`Низкий ценовой`);
+      },
+      high: () => {
+        console.log(`Высокий ценовой`);
       }
+    },
+    'housing-rooms': {
+      1: f,
+      2: f,
+      3: f
+    },
+    'housing-guests': {
+      2: f,
+      1: f,
+      0: f
     }
-    // else if (evt.target.id === `housing-price`) {
-    //   console.log(pins);
-    //   console.log(evt.target.value);
+  };
 
-    //   if (evt.target.value === `middle`) {
-    //     window.pin.getRemovePins();
-    //     const sortedPins = pins.filter((pin) => {
-    //       return (pin.offer.price >= 10000) && (pin.offer.price <= 50000);
-    //     });
+  const changeFunction = (evt) => {
+    // let pins = window.data.serverData.slice();
+    window.pin.getRemovePopup();
+    selectObject[evt.target.id][evt.target.value]();
+  };
 
-    //     window.map.getPinMap(sortedPins);
-    //   }
-    // }
+  mapFilters.addEventListener(`change`, changeFunction);
+  // mapFilters.addEventListener(`change`, (evt) => {
+  //   let pins = window.data.serverData.slice();
+  //   window.pin.getRemovePopup();
+  //   console.log(selectObject[evt.target.id][evt.target.value]);
+  //   if (evt.target.id === `housing-type`) {
+  //     if (evt.target.value !== `any`) {
+  //       window.pin.getRemovePins();
 
-  });
+  //       const sortedPins = pins.filter((pin) => {
+  //         return pin.offer.type === evt.target.value;
+  //       });
+
+  //       window.map.getPinMap(sortedPins);
+  //     } else {
+  //       window.pin.getRemovePins();
+  //       window.map.getPinMap(pins);
+  //     }
+  //   }
+  // else if (evt.target.id === `housing-price`) {
+  //   console.log(pins);
+  //   console.log(evt.target.value);
+
+  //   if (evt.target.value === `middle`) {
+  //     window.pin.getRemovePins();
+  //     const sortedPins = pins.filter((pin) => {
+  //       return (pin.offer.price >= 10000) && (pin.offer.price <= 50000);
+  //     });
+
+  //     window.map.getPinMap(sortedPins);
+  //   }
+  // }
+
+  // });
 
 
   // const sortedCb = (pins) => {
