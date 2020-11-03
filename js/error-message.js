fe01fdad547bc345c7a3defcb38c59813c22dae9
korbutds <sys@hohou.ru>
2020-11-03 09:55:34 +0300

    Подчистил код от комментариев

--- js/error-message.js
+++ js/error-message.js
@@ -46,37 +46,3 @@ const errorUploadOn = (errorText) => {
 window.error = {
   errorUploadOn
 };
-
-
-// const errorTemplate = document.querySelector(`#error`).content.querySelector(`.error`);
-
-// window.errorMessage = (errorText, flag) => {
-//   const errorFragment = document.createDocumentFragment();
-//   const newErrMessage = errorTemplate.cloneNode(true);
-//   newErrMessage.querySelector(`.error__message`).textContent = errorText;
-//   errorFragment.appendChild(newErrMessage);
-//   document.querySelector(`.map__pins`).appendChild(errorFragment);
-//   const errorButton = newErrMessage.querySelector(`.error__button`);
-
-//   const onErrorButtonClick = () => {
-//     newErrMessage.remove();
-//     errorButton.removeEventListener(`click`, onErrorButtonClick);
-//     if (!flag) {
-//       window.pageActivate.getDeactivePage();
-//     }
-//   };
-
-//   const outOfErrorMessage = () => {
-//     return (evt) => {
-//       if (evt.code === `Escape` || evt.button === 0) {
-//         onErrorButtonClick();
-
-//         document.removeEventListener(`mousedown`, outOfErrorMessage());
-//         document.removeEventListener(`keydown`, outOfErrorMessage());
-//       }
-//     };
-//   };
-//   errorButton.addEventListener(`click`, onErrorButtonClick);
-//   document.addEventListener(`keydown`, outOfErrorMessage());
-//   document.addEventListener(`mousedown`, outOfErrorMessage());
-// };
